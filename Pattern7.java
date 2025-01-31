db813c6298754c2ba7e41a15809ac73b88f7ab96
Sindhugoud03 <sindhugoudlingaram@gmail.com>
2025-02-01 01:21:30 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,29 @@
+public class Pattern7{
+    public static void main(String[] args) {
+        for (int i = 1;i<= 3 ; i++) {
+            for(int j=1;j<=3-i;j++){
+                System.out.print(' ');
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+            
+        }
+        for(int i=3; i>0;i--){
+            for(int j=0; j<5-i;j++){
+                System.out.print(' ');
+            }
+            for(int j=0;j<5-i;j++){
+                System.out.print(' ');
+            }
+            for(int j=0;j<i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+
+}
